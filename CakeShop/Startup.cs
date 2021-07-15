457d3216389f479df51ac35dec1997f7114e458d
Jyothish <jyothishmb1992@gmail.com>
2021-07-15 22:55:23 +0530

    After initial migration.

--- CakeShop/Startup.cs
+++ CakeShop/Startup.cs
@@ -27,9 +27,11 @@ namespace CakeShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => 
+            {
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"), 
+                b => b.MigrationsAssembly("CakeShop.DataAccess"));
+            });
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
