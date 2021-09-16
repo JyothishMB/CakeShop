c8901115d9419c225ce86e79907d32b2c7ce1c81
Jyothish <jyothishmb1992@gmail.com>
2021-09-17 00:54:16 +0530

    Removing old MVC and related projects completely.

--- CakeShop.DataAccess/Repository/IRepository/IGiftPackRepository.cs
+++
@@ -1,9 +0,0 @@
-using CakeShop.Models;
-
-namespace CakeShop.DataAccess.Repository.IRepository
-{
-    public interface IGiftPackRepository : IRepository<GiftPack>
-    {
-         void Update(GiftPack giftPack);
-    }
-}
